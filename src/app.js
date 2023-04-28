dcdfdee3ab6f95818409a9978ed80c2161681e6e
aleksandra-gab <thatsaleksandra@gmail.com>
2023-04-28 18:40:29 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -44,7 +44,19 @@ function displayWeather(response) {
   icon.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "739473ae0tafe02b875bc88cd82o1460";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=milan&key=${apiKey}`;
+function search(city) {
+  let apiKey = "739473ae0tafe02b875bc88cd82o1460";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
+}
 
-axios.get(apiUrl).then(displayWeather);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
+}
+
+search("Milan");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
